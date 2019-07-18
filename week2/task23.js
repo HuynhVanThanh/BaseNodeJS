783edddeaafc68a288256787480d82d2e4d7f5ba
HuynhVanThanh <52908729+HuynhVanThanh@users.noreply.github.com>
2019-07-18 12:33:22 +0700

    week2 - task23 (complete)

--- week2/task23.js
+++ week2/task23.js
@@ -1,3 +1,4 @@
+///////////////////////// Task2 ////////////////////////////////////
 
 const fs = require('fs');
 // async
@@ -11,30 +12,32 @@ const fs = require('fs');
 // sync
 try {
 const data = fs.readFileSync('./products.json', 'utf8');
-// console.log(data);
  products = JSON.parse(data);
-
 } catch (err) {
 console.error(err);
 }
 
 console.log('tong so san pham: ' + products.length);
 
-const moment = require('moment');
 products.forEach(element => {
-    // element.dateUpdated = new Date();
     element.dateUpdated = new Date(element.dateUpdated);
 });
 
+const moment = require('moment');
+moment.updateLocale('vi', {relativeTime : Object});
+var numeral = require('numeral');
+
 products.forEach(element => {
-    console.log(element.id + ' - ' + element.name + ' - ' 
-    + element.price + ' - ' + 'Cập nhật cách đây ' + moment(element.dateUpdated).fromNow());
-    // 0001 - iPhone X - 30,000,000VND - Cập nhật cách đây 3 ngày
+    var id = element.id;
+    var name = element.name;
+    var price = numeral(parseInt(element.price)).format('0,0');
+    var fromNow = moment(element.dateUpdated).fromNow();
+    console.log(`${id} - ${name} - ${price} VND - Cap nhat cach day ${fromNow}`);
 });
 
 ///////////////////////// Task3 ////////////////////////////////////
 
-var format = require('date-fns/format')
+var format = require('date-fns/format');
 
 products.forEach(element => {
     element.updated = element.dateUpdated;
@@ -42,10 +45,22 @@ products.forEach(element => {
     delete element.dateUpdated;
 });
 
-// excel ./products.json products;
+const XLSX = require('xlsx');
+// create 'worksheet' object from json
+const ws = XLSX.utils.json_to_sheet(products);
+// Optional: config columns width (character length)
+ws['!cols'] = [{ width: 20 }, { width: 25 }, { width: 20 }, { width: 20 }, { width: 20 }];
+// create 'workbook' object (which contains multiple sheet)
+const wb = XLSX.utils.book_new();
+XLSX.utils.book_append_sheet(wb, ws, 'Products');
+// convert to Microsoft EXCEL workbook and write to a Buffer object
+const buf = XLSX.write(wb, { type: 'buffer', bookType: 'xlsx' });
 
-console.log(products[1]);
-
-
+const writeToHardDrive = buf => {
+  fs.writeFile('products.xlsx', buf, err => {
+    console.log('Write success');
+  });
+};
+writeToHardDrive(buf);
 
 
