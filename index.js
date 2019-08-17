7bf7b08bf73d6f7c7b1326ad3fd016f527eb7792
HuynhVanThanh <52908729+HuynhVanThanh@users.noreply.github.com>
2019-08-17 20:32:05 +0700

    assignment3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const fs = require('fs');
 
-const port = 3000 || nodejs-heroku256.herokuapp.com
+const port = 3000
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
   app.use(express.static('public'))  
