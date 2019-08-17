70260609dcd2f6c6a68bffeade88ac4fc34fc7cf
HuynhVanThanh <52908729+HuynhVanThanh@users.noreply.github.com>
2019-08-17 21:58:10 +0700

    assingment3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const fs = require('fs');
+const fs = require('fs')
 
 const port = 3000
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
