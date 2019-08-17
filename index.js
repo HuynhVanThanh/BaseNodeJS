a2b0623ebd95aaaad9d84890cf95bbd9a603cbf9
HuynhVanThanh <52908729+HuynhVanThanh@users.noreply.github.com>
2019-08-17 16:12:34 +0700

    assingment3

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express')
 const app = express()
 const fs = require('fs');
 
-const port = 3000
-  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const port = 3000 || nodejs-heroku256.herokuapp.com
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
   app.use(express.static('public'))  
 //   app.use('/static', express.static(path.join(__dirname, 'public')))
